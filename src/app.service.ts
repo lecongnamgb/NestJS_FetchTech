89e0a73d1c477fc80477de006468ad9a5ff84588
Le Cong Nam <lecongnamgb@gmail.com>
2022-06-08 14:30:02 +0700

    add basic sign in feature

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,39 @@
+import { CreateUserDTO } from './modules/users/dto/create-user.dto';
+import { UserSchema, UserDocument } from './schemas/user.schema';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+
+const saltOrRounds = 10;
 
 @Injectable()
 export class AppService {
+  constructor(@InjectModel('User') private userModel: Model<UserDocument>) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async signIn(createUserDTO: CreateUserDTO): Promise<any> {
+    try {
+      const user = await this.userModel.findOne({
+        username: createUserDTO.username,
+      });
+      if (user) {
+        return 'Username has already existed! Please try another username';
+      }
+      const hashpw = await bcrypt.hash(createUserDTO.password, saltOrRounds);
+      console.log(hashpw);
+      const createdUser = await this.userModel.create({
+        username: createUserDTO.username,
+        password: hashpw,
+      });
+      if (!createdUser) {
+        return `There are some problems when creating user!`;
+      }
+      return `Create User successfully`;
+    } catch (err) {
+      console.log(`Whoop! ${err}`);
+    }
   }
 }
---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+@Schema()
+export class User {
+  @Prop({ type: String, required: true })
+  username: string;
+
+  @Prop({ type: String, required: true })
+  password: string;
+
+  @Prop({ type: String })
+  refreshToken: string;
+}
+
+export type UserDocument = User & Document;
+
+export const UserSchema = SchemaFactory.createForClass(User);
